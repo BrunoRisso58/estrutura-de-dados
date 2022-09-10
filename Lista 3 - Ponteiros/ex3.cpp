675b72196f19e14b7d8d26cc0994ff60240b812c
Bruno Risso <brunorisso58@gmail.com>
2022-09-10 00:11:39 -0300

    Exercício 3

---
+++ Lista 3 - Ponteiros/ex3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <string.h>
+#include <locale>
+using namespace std;
+
+// a) O código compila? Sim
+// b) Após executar o código, qual será o valor de inx? 20
+// c) Após executar o código, para onde parr estará apontando? Para o endereço de memória de arr[4]
+
+int main() {
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	int arr[5] = { 10, 80, 40, 30, 20 };
+	int *parr = &arr[4];
+	int inx = 0;
+	
+	inx = *parr++;
+	
+	cout << inx;
+	
+	
+	
+}
